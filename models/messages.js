0d258be6fb18c423c761d2104f29b07f983b868e
Karlgrund <jkarlgrund@gmail.com>
2015-04-27 22:25:55 +0200

    Partially added capability to add information to db.

--- models/messages.js
+++ models/messages.js
@@ -1,8 +1,18 @@
 var db = require('../lib/database');
 
 exports.get = function (callback) {
-    db.get(function (err, data) {
+    db.getMessages(function (err, data) {
         if (err) return callback(err, null);
         callback(null, data);
     });
-}
+};
+
+exports.add = function (req, res, next) {
+    db.addMessage(function (err, data) {
+        res.json(err ? 503 : 200, {
+            error: err ? true : null,
+            errorMessage: err ? err : null,
+            data: data
+        });
+    });
+};
