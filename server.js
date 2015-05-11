50f5174879749f7baf7354d915d5bca882a3284c
Karlgrund <jkarlgrund@gmail.com>
2015-05-11 22:29:02 +0200

    Added possibility to update client with given time intervall. Not working for wodify yet

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var io = require('./controllers/socketHandler');
 var bodyParser = require('body-parser');
 var messages = require('./models/messages');
 var logger = require('./lib/logger')
+var timetable = require('./lib/timetable');
 
 io.listen(server);
 app.use(bodyParser.json());
@@ -30,5 +31,9 @@ process.argv.forEach(function(val) {
 app.post('/addMessage', messages.add, io.updateAllUsers);
 
 server.listen(8080, function() {
+    timetable.addTimer("SL", function(err) {
+        if(err) console.log("ERROR: " + err);
+        console.log("Now we should send some data to clients");
+    });
     console.log('[SERVER] Server listening on localhost:8080');
 });
