353e15bca8d0e556a9ad0a3f708c68fc119168e9
Johan Karlgrund <johan.karlgrund@ericsson.com>
2015-05-29 14:18:36 +0200

    Update for Heroku

--- controllers/keys.js
+++ controllers/keys.js
@@ -1,9 +1,11 @@
 var APIKey = require('../API_keys');
 
 exports.getWodifyAPI = function() {
+    if(process.env.wodify) return process.env.wodify;
     return APIKey.wodify;
 }
 
 exports.getSLAPI = function() {
+    if(process.env.wodify) return process.env.SL;
     return APIKey.SL;
 }
