12990894df5b7bc19ade8348ca859a3cfd4170ca
Johan Karlgrund <johan.karlgrund@ericsson.com>
2015-08-26 15:38:25 +0200

    Adding error handling to server.js on startup. Switching dev port from 8080 to 8081

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = require('express')(),
     server = require('http').Server(app),
     logger = require('./lib/logger');
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8081;
 
 process.argv.forEach(function(val) {
     switch(val) {
@@ -25,7 +25,10 @@ require('./config/express')(app, server);
 require('./lib/db').init('ds039321.mongolab.com', 39321, function(err) {
     if(err) throw err;
     logger.log("Mongoose database connected");
-    server.listen(port, function() {
+    server.listen(port, function(e) {
+        if(e) {
+            logger.logError("Error in startup: " + e);
+        }
         logger.log("Server listening on http://localhost:" + port);
         console.timeEnd("Starting app");
     });
